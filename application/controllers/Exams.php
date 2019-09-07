a1cfedddce5c1e16b2813baefc35b07ed9f04501
neelsaha <neel.saha1995@gmail.com>
2019-09-07 21:03:43 +0530

    addExam

--- application/controllers/Exams.php
+++ application/controllers/Exams.php
@@ -38,6 +38,26 @@ class Exams extends CI_Controller {
         $this->inputoutput->setResponse($aStatusCode,$aResponse);
     }
 
+    public function addExam($iClass,$iSection){
+        TRC_LOG('debug',"Inside addExam");
+        $aToken = substr($this->input->get_request_header('Authorization', TRUE),7);
+        $aStatusCode = 500;
+        $aResponse = array();
+        if($this->sesssecurity->checkSession($aStatusCode, $aToken, 'teacher')){
+            try{
+                $aData = $this->inputoutput->getRequest();
+                $this->load->model('ExamsModel');
+                $aResponse = $this->ExamsModel->setExamDetails($iClass,$iSection,$aData,$aStatusCode);
+            }catch(Exception $e){
+                TRC_LOG('error',$e->getMessage());
+                $aStatusCode = 500;
+            }
+        }else{
+            TRC_LOG('debug','Unaothorized access');
+        }
+        $this->inputoutput->setResponse($aStatusCode,$aResponse);
+    }
+
 }
 
 ?>
